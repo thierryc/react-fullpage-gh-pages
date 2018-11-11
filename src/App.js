5bd99c73023e5cfb880417a5c989bb88741495ad
Thierry Charbonnel <thierryc@users.noreply.github.com>
2018-11-11 17:20:44 -0500

    add 2 pages

--- src/App.js
+++ src/App.js
@@ -6,22 +6,69 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <React.StrictMode>
+        <Fullpage>
+
+          <FullpageNavigation/>
+
+          <FullPageSections>
+            <FullpageSection>
+              <div className="App">
+                <header className="App-header">
+                  <img src={logo} className="App-logo" alt="logo" />
+                  <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                  </p>
+                  <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Learn React
+                  </a>
+                </header>
+              </div>
+            </FullpageSection>
+            <FullpageSection>
+              <div className="App">
+                <header className="App-header">
+                  <img src={logo} className="App-logo" alt="logo" />
+                  <p>
+                    Fullpage <FullpageSection.Number/>/<FullpageCount/>.
+                  </p>
+                  <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Learn React
+                  </a>
+                </header>
+              </div>
+            </FullpageSection>
+            <FullpageSection>
+              <div className="App">
+                <header className="App-header">
+                  <img src={logo} className="App-logo" alt="logo" />
+                  <p>
+                    Fullpage <FullpageSection.Number/>/<FullpageCount/>.
+                  </p>
+                  <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Learn React
+                  </a>
+                </header>
+              </div>
+            </FullpageSection>
+          </FullPageSections>
+        </Fullpage>
+      </React.StrictMode>
     );
   }
 }
