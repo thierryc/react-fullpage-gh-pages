96be0cb1a1b59766bdc6e1aeecdf584d11bf2480
Thierry Charbonnel <t@ap.cx>
2022-07-31 17:48:09 -0400

    app btn link

--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 import {
   useLocation,
 } from "react-router-dom";
-import Fullpage, { FullPageSections, FullpageSection, FullpageCount, FullpageNavigation } from '@ap.cx/react-fullpage';
+import Fullpage, { FullPageSections, FullpageSection, FullpageCount, FullpageNavigation, FullpageContext } from '@ap.cx/react-fullpage';
 import logo from './logo.svg';
 
 function Home() {
@@ -19,16 +19,15 @@ function Home() {
             <header className="App-header">
               <img src={logo} className="App-logo" alt="logo" />
               <p>
-                React FullPage
+                Fullpage <FullpageSection.Number/>/<FullpageCount/>.
               </p>
-              <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Learn React
-              </a>
+              <FullpageContext.Consumer>
+                {
+                  ctx => (
+                    <button onClick={() => ctx.goto(ctx.slides[ctx.slides.length -1])}>Goto Last</button>
+                  )
+                } 
+              </FullpageContext.Consumer>
             </header>
           </div>
         </FullpageSection>
@@ -39,14 +38,13 @@ function Home() {
               <p>
                 Fullpage <FullpageSection.Number/>/<FullpageCount/>.
               </p>
-              <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Learn React
-              </a>
+              <FullpageContext.Consumer>
+                {
+                  ctx => (
+                    <button onClick={() => ctx.goto(ctx.slides[0])}>Goto First</button>
+                  )
+                }
+              </FullpageContext.Consumer>
             </header>
           </div>
         </FullpageSection>
@@ -57,14 +55,13 @@ function Home() {
               <p>
                 Fullpage <FullpageSection.Number/>/<FullpageCount/>.
               </p>
-              <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Learn React
-              </a>
+              <FullpageContext.Consumer>
+                {
+                  ctx => (
+                    <button onClick={() => ctx.goto(ctx.slides[0])}>Goto First</button>
+                  )
+                } 
+              </FullpageContext.Consumer>
             </header>
           </div>
         </FullpageSection>
